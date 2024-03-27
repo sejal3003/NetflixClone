273292827c08f701ea2b0f32b8a1f6b8c70bac8e
Sejal <rampuresejal@gmail.com>
2024-03-27 19:18:45 +0530

    added the api folder

---
+++ controllers/UserController.js
@@ -0,0 +1,24 @@
+const User = require("../models/UserModel");
+
+module.exports.addToLikedMovies = async (req, res) => {
+  try {
+    const { email, data } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const { likedMovies } = user;
+      const movieAlreadyLiked = likedMovies.find(({ id }) => id === data.id);
+      if (!movieAlreadyLiked) {
+        await User.findByIdAndUpdate(
+          user._id,
+          {
+            likedMovies: [...user.likedMovies, data],
+          },
+          { new: true }
+        );
+      } else return res.json({ msg: "Movie already added to the liked list." });
+    } else await User.create({ email, likedMovies: [data] });
+    return res.json({ msg: "Movie successfully added to liked list." });
+  } catch (error) {
+    return res.json({ msg: "Error adding movie to the liked list" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//connect to MongoDb
+mongoose
+  .connect("mongodb://localhost:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("server started on port 5000"));
