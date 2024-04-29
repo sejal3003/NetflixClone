708f7ad9ea03e65c94554750dcfb71f9a96192aa
Sejal <rampuresejal@gmail.com>
2024-04-29 14:12:13 +0530

    created the usersdata and moviesdata table in Admin Portal

--- src/AdminPages/AdminMovies.js
+++ src/AdminPages/AdminMovies.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../AdminPages/adMovies.css";
 
-export default function AdminMovies() {
+const AdminMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieData = async () => {
+    try {
+      const logindataString = localStorage.getItem("loginData");
+      const logindata = JSON.parse(logindataString);
+      const token = logindata.token;
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const response = await axios.get(
+        "http://localhost:8000/api/admin/getmovies",
+        {
+          headers: headers,
+        }
+      );
+
+      setMovies(response.data);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
   return (
-    <div>
-      <h1>Movies</h1>
-    </div>
+    <section className="admin-movies-section">
+      <div className="moviecontainer">
+        <h1>Admin Movies Data</h1>
+      </div>
+      <div className="moviecontainer admin-mov">
+        <div className="movie-list-container">
+          <table className="movie-list">
+            <thead>
+              <tr>
+                <th>Movie Id</th>
+                <th>Movie Name</th>
+                <th>Movie Image</th>
+                <th>Movie Genre</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((movie, index) => (
+                <tr key={index}>
+                  <td>{movie.id}</td>
+                  <td>{movie.name}</td>
+                  <td>{movie.image}</td>
+                  <td>{movie.genre}</td>
+                  <td>Delete</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default AdminMovies;
--- src/AdminPages/AdminUsers.js
+++ src/AdminPages/AdminUsers.js
@@ -1,9 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../AdminPages/adUsers.css";
 
 export default function AdminUsers() {
+  const [users, setUsers] = useState([]);
+  const fetchData = async () => {
+    try {
+      // Retrieve the logindata object from localStorage
+      const logindataString = localStorage.getItem("loginData");
+
+      // Parse the JSON string into a JavaScript object
+      const logindata = JSON.parse(logindataString);
+
+      // Extract the token from the logindata object
+      const token = logindata.token;
+
+      // Now you can use the token for your Axios request
+      console.log("Token:", token);
+
+      const headers = {
+        Authorization: `Bearer ${token}`, // Assuming your backend expects a Bearer token
+      };
+
+      // Make GET request to your backend API
+      const response = await axios.get(
+        "http://localhost:8000/api/admin/users",
+        {
+          headers: headers,
+        }
+      );
+
+      // Handle the response data
+      console.log("Data from backend:", response.data);
+
+      // return response.data;
+      setUsers(response.data);
+    } catch (error) {
+      // Handle errors
+      console.error("Error fetching data:", error);
+      throw error; // Optional: propagate the error
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //Delete the user onclick of the delete button
+
+  const deleteUser = async (id) => {
+    try {
+      // Make DELETE request to delete user by ID
+      const logindataString = localStorage.getItem("loginData");
+      const logindata = JSON.parse(logindataString);
+      const token = logindata.token;
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      await axios.delete(`http://localhost:8000/api/admin/users/delete/${id}`, {
+        headers: headers,
+      });
+
+      // Re-fetch the users
+      fetchData();
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello users</h1>
-    </div>
+    <>
+      <section className="admin-users-section">
+        <div className="userContainer">
+          <h1>Admin Users Data</h1>
+        </div>
+        <div className="userContainer admin-users">
+          <table>
+            <thead>
+              <tr>
+                <th> User Id</th>
+                <th>Email</th>
+                <th>CreatedAt</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((curUser, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{curUser._id}</td>
+                    <td>{curUser.email}</td>
+                    <td>{curUser.createdAt}</td>
+                    <td>
+                      <button
+                        className="delete-btn"
+                        onClick={() => deleteUser(curUser._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
   );
 }
