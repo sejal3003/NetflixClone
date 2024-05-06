6ae5701b6d531d5197e75e7d7465c6bf190414d8
Sejal <rampuresejal@gmail.com>
2024-05-06 16:51:17 +0530

    Provided the checkout to the plan page itself

--- src/components/PlanCard.jsx
+++ src/components/PlanCard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "../styles/PCard.css";
 
-export default function PlanCards({ title, price, text, title2 }) {
+export default function PlanCards({
+  title,
+  price,
+  text,
+  title2,
+  handlePayment,
+}) {
+  const handleClick = () => {
+    handlePayment(price);
+  };
   return (
-    <div className="card zoom">
+    <div className="card">
       <h2 className="card-title">{title}</h2>
-      <h6 className="price">{price}</h6>
-      <h5 className="card`-title2">{title2}</h5>
+      <h5 className="card-title2">{title2}</h5>
       <p className="card-text">{text}</p>
+      <h6 className="price">Monthly ₹{price}</h6>
+      <button className="selectPlan" onClick={handleClick}>
+        Select Plan
+      </button>
     </div>
   );
 }
--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,60 +1,148 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
 import "../styles/Plan.css";
-import PlanCard from "../components/PlanCard";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PlanCard from "../components/PlanCard"; // Import the PlanCard component
 
-const Plan = () => {
+export default function Payment() {
   const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [isTransactionSuccessful, setIsTransactionSuccessful] = useState(false);
+  const [isNextButtonVisible, setIsNextButtonVisible] = useState(false);
+
+  const currency = "INR";
+  const receiptId = "qwsql";
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePaymentSuccess = () => {
+    setIsTransactionSuccessful(true);
+    setIsNextButtonVisible(true); // Show the Next button after successful transaction
+  };
+
+  const paymentHandler = async (amount) => {
+    const paiseAmount = amount * 100; // Convert rupees to paise
+    const response = await fetch("http://localhost:8000/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: paiseAmount,
+        currency,
+        receipt: receiptId,
+      }),
+    });
+    const order = await response.json();
+    console.log(order);
+
+    var option = {
+      key: "",
+      amount: paiseAmount,
+      currency,
+      name: "Netflix Subscription",
+      description: "Test Transaction",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLBoNFiGLLn1OUzqhNveglzC5uGYa8U1o3Sw&s",
+      order_id: order.id,
+      handler: async function (response) {
+        toast.success("Transaction Successful");
+        alert("Transaction successful");
+
+        const body = { ...response };
+
+        const validateResponse = await fetch("http://localhost:8000/validate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        const jsonResponse = await validateResponse.json();
+
+        console.log("jsonResponse", jsonResponse);
+        handlePaymentSuccess();
+      },
+      prefill: {
+        name: name, // Dynamic name
+        email: "sej@example.com",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var rzp1 = new window.Razorpay(option);
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+
+    rzp1.open();
+  };
+
   return (
     <div className="plans">
-      <div className="content">
+      <div className="planContent">
         <Header />
         <div className="d-flex justify-content-center">
           <div className="planPageBody">
             <i
               className="fas fa-check-circle"
-              style={{ color: "red", fontSize: "25px", padding: "9px" }}
+              style={{ color: "red", fontSize: "25px", padding: "20px" }}
             ></i>
-            <h5>STEP 2 OF 3</h5>
+            <h5>STEP 2 OF 2</h5>
             <h2>Choose the plan thats right for you</h2>
-
-            <div className="subscription-cards  ">
+            <p>
+              "Your payment is encrypted and you can change your payment method
+              at anytime".
+            </p>
+            <div className="subscription-cards">
+              {/* PlanCard components with fixed prices */}
               <PlanCard
-                title="Basic Plan "
-                price="{Monthly price ₹199}"
-                text="Supported devices
-                 TV, computer, mobile phone, tablet 
-                "
-                title2="Resolution 720p (HD)"
+                title="Basic Plan"
+                price={199}
+                text="Supports 1 device to watch at same time (mobile phone)
+              "
+                title2="Resolution HD"
+                handlePayment={paymentHandler} // Pass the payment handler function
               />
-
               <PlanCard
                 title="Standard Plan"
-                price="{Monthly price ₹499}"
-                text="Supported devices
-                      TV, computer, mobile phone, tablet"
-                title2="Resolution
-                        1080p (Full HD)"
+                price={499}
+                text="Supports 2 device to watch (mobile phone,computer)
+              "
+                title2="Resolution Full HD"
+                handlePayment={paymentHandler} // Pass the payment handler function
               />
               <PlanCard
                 title="Premium Plan"
-                price="{Monthly price ₹699}"
-                text="Supported devices
-                        TV, computer, mobile phone, tablet"
-                title2="Resolution
-                     4K (Ultra HD) + HDR"
+                price={799}
+                text="Supports 4 devices to watch mobile phone,TV,Laptop,Tablet)"
+                title2="Resolution 4K (Ultra HD) + HDR"
+                handlePayment={paymentHandler} // Pass the payment handler function
               />
             </div>
-            <button className="nextButton" onClick={() => navigate("/payment")}>
-              Next
-            </button>
+            {isNextButtonVisible && (
+              <button className="nextButton" onClick={() => navigate("/login")}>
+                Next
+              </button>
+            )}
           </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default Plan;
+}
